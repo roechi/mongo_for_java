b0949dc8beaaba4c083dd049af05cc72f3a215eb
roechi <msg.remus@gmail.com>
2016-06-13 13:32:42 +0200

    adds homework 3.1

--- week_2_crud/create_blog_authors/src/main/java/course/UserDAO.java
+++ week_2_crud/create_blog_authors/src/main/java/course/UserDAO.java
@@ -20,6 +20,7 @@ import com.mongodb.ErrorCategory;
 import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import sun.misc.BASE64Encoder;
 
@@ -48,15 +49,20 @@ public class UserDAO {
         // create an object suitable for insertion into the user collection
         // be sure to add username and hashed password to the document. problem instructions
         // will tell you the schema that the documents must follow.
+        Document user = new Document()
+                .append("_id", username)
+                .append("password", passwordHash);
 
         if (email != null && !email.equals("")) {
             // XXX WORK HERE
             // if there is an email address specified, add it to the document too.
+            user.append("email", email);
         }
 
         try {
             // XXX WORK HERE
             // insert the document into the user collection here
+            usersCollection.insertOne(user);
             return true;
         } catch (MongoWriteException e) {
             if (e.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
@@ -72,6 +78,7 @@ public class UserDAO {
 
         // XXX look in the user collection for a user that has this username
         // assign the result to the user variable.
+        user = usersCollection.find(Filters.eq("_id", username)).first();
 
         if (user == null) {
             System.out.println("User not in database");
